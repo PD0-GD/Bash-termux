54fb910a93b3863d69bbcf05c0fde25675e7aa91
PD0-GD <116287859+PD0-GD@users.noreply.github.com>
2023-10-16 19:01:35 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -6,3 +6,13 @@ echo -e "\e[0;35m
 ██╔══██╗██╔══██║░╚═══██╗██╔══██║
 ██████╦╝██║░░██║██████╔╝██║░░██║
 ╚═════╝░╚═╝░░╚═╝╚═════╝░╚═╝░░╚═╝\e[0m"
+echo "1 = python, 2 = update and upgrade apt"
+read choice
+
+if [ "${choice}" -eq 1" ]; then
+    apt install python python2 python3
+elif [ "${choice}" -eq 2" ]; then
+    apt update && apt upgrade
+else
+    echo "\e[0;31mchoose a valid number!\e[0m"
+fi
