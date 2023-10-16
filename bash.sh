c83c59f6d7a174f1d07ce3c4cfc1c26a6a948a91
PD0-GD <116287859+PD0-GD@users.noreply.github.com>
2023-10-16 19:31:50 +0100

    Added an all option
    Yes

--- bash.sh
+++ bash.sh
@@ -6,7 +6,7 @@ echo -e "\e[0;35m
 ██╔══██╗██╔══██║░╚═══██╗██╔══██║
 ██████╦╝██║░░██║██████╔╝██║░░██║
 ╚═════╝░╚═╝░░╚═╝╚═════╝░╚═╝░░╚═╝\e[0m"
-echo "1 = python, 2 = update and upgrade apt, 3 = apache"
+echo "1 = python, 2 = update and upgrade apt, 3 = apache, 4 = all"
 read choice
 
 if [ "${choice}" -eq 1 ]; then
@@ -14,6 +14,10 @@ if [ "${choice}" -eq 1 ]; then
 elif [ "${choice}" -eq 2 ]; then
     apt update && apt upgrade
 elif [ "${choice}" -eq 3 ]; then
+    apt install apache2
+elif [ "${choice}" -eq 4 ]; then
+    apt install python python2 python3
+    apt update && apt upgrade
     apt install apache2
 else
     echo -e "\e[0;31mchoose a valid number!\e[0m"
