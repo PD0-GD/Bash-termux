937a714b08664f58c4a0f9491c33e2ec61efee39
PD0-GD <116287859+PD0-GD@users.noreply.github.com>
2023-10-16 19:45:17 +0100

    .
    Why did you click this ignore this

--- bash.sh
+++ bash.sh
@@ -11,11 +11,9 @@ read choice
 
 if [ "${choice}" -eq 1 ]; then
     apt install python python2 python3
-    cd /data/data/com.termux/files/home
     clear
 elif [ "${choice}" -eq 2 ]; then
     apt update && apt upgrade
-    cd /data/data/com.termux/files/home
     clear
 elif [ "${choice}" -eq 3 ]; then
     apt install apache2
@@ -25,7 +23,6 @@ elif [ "${choice}" -eq 4 ]; then
     apt install python python2 python3
     apt update && apt upgrade
     apt install apache2
-    cd /data/data/com.termux/files/home
     clear
 else
     echo -e "\e[0;31mchoose a valid number!\e[0m"
