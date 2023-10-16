1458891451b922fb51bb4cc2f6067f3d3bdaf765
PD0-GD <116287859+PD0-GD@users.noreply.github.com>
2023-10-16 19:20:27 +0100

    Apache bash.sh
    Added apache

--- bash.sh
+++ bash.sh
@@ -6,13 +6,15 @@ echo -e "\e[0;35m
 ██╔══██╗██╔══██║░╚═══██╗██╔══██║
 ██████╦╝██║░░██║██████╔╝██║░░██║
 ╚═════╝░╚═╝░░╚═╝╚═════╝░╚═╝░░╚═╝\e[0m"
-echo "1 = python, 2 = update and upgrade apt"
+echo "1 = python, 2 = update and upgrade apt, 3 = apache"
 read choice
 
 if [ "${choice}" -eq 1 ]; then
     apt install python python2 python3
 elif [ "${choice}" -eq 2 ]; then
     apt update && apt upgrade
+elif [ "${choice}" -eq 3 ]; then
+    apt install apache2
 else
     echo -e "\e[0;31mchoose a valid number!\e[0m"
 fi
