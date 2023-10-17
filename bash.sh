e48aacb85a94e29f13414ac186685a91c1f42812
PD0-GD <116287859+PD0-GD@users.noreply.github.com>
2023-10-17 22:02:12 +0100

    Update bash.sh
    Php

--- bash.sh
+++ bash.sh
@@ -6,7 +6,7 @@ echo -e "\e[0;35m
 ██╔══██╗██╔══██║░╚═══██╗██╔══██║
 ██████╦╝██║░░██║██████╔╝██║░░██║
 ╚═════╝░╚═╝░░╚═╝╚═════╝░╚═╝░░╚═╝\e[0m"
-echo "1 = python, 2 = update and upgrade apt, 3 = apache, 4 = all, 5 = update"
+echo "1 = python, 2 = update and upgrade apt, 3 = apache, 4 = php stuff, 5 = all, 6 = update"
 read choice
 
 if [ "${choice}" -eq 1 ]; then
@@ -20,11 +20,15 @@ elif [ "${choice}" -eq 3 ]; then
     cd /data/data/com.termux/files/home
     clear
 elif [ "${choice}" -eq 4 ]; then
+    apt install php php-apache phpmyadmin  mariadb
+elif [ "${choice}" -eq 5 ]; then
     apt install python python2 python3
     apt update && apt upgrade
     apt install apache2
+    apt install php php-apache phpmyadmin  mariadb
+    ~/Bash-termux/update.sh
     clear
-elif [ "${choice}" -eq 5 ]; then
+elif [ "${choice}" -eq 6 ]; then
     ~/Bash-termux/update.sh
 else
     echo -e "\e[0;31mchoose a valid number!\e[0m"
