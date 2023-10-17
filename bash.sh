775ca8772ce26359d2b0d229cd769e6d9a84e095
PD0-GD <116287859+PD0-GD@users.noreply.github.com>
2023-10-17 15:20:28 +0100

    Update bash.sh
    Added update

--- bash.sh
+++ bash.sh
@@ -6,7 +6,7 @@ echo -e "\e[0;35m
 ██╔══██╗██╔══██║░╚═══██╗██╔══██║
 ██████╦╝██║░░██║██████╔╝██║░░██║
 ╚═════╝░╚═╝░░╚═╝╚═════╝░╚═╝░░╚═╝\e[0m"
-echo "1 = python, 2 = update and upgrade apt, 3 = apache, 4 = all"
+echo "1 = python, 2 = update and upgrade apt, 3 = apache, 4 = all, 5 = update"
 read choice
 
 if [ "${choice}" -eq 1 ]; then
@@ -24,6 +24,8 @@ elif [ "${choice}" -eq 4 ]; then
     apt update && apt upgrade
     apt install apache2
     clear
+elif [ "${$choice}" -eq 5 ]; then
+    ~/Bash-termux/update.sh
 else
     echo -e "\e[0;31mchoose a valid number!\e[0m"
 fi
